b8701e9a229135742020c96ffb0f17be66402d64
Frank Steiner <fsteiner@isx-001.lan.isx-software.de>
2015-02-17 09:41:02 +0100

    Initial Commit

---
+++ PlsPlay/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PlsPlay
+//
+//  Created by Frank Steiner on 17.02.15.
+//  Copyright (c) 2015 i.S.X. Software. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
